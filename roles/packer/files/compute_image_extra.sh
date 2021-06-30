09bb3fa07c2ae358bce229db9e1e31b2483dbe04
Will Furnass <w.furnass@sheffield.ac.uk>
2021-06-30 11:25:33 +0100

    Ensure image built w/ packer has dev tools and modules setup in

--- roles/packer/files/compute_image_extra.sh
+++ roles/packer/files/compute_image_extra.sh
@@ -9,3 +9,8 @@
 
 # to install CernVM-FS and configure access to EESSI, uncomment the line below:
 # /home/citc/install_cvmfs_eessi.sh
+
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install cmake gcc-gfortran
+sudo yum -y install openssl-devel
+echo 'export MODULEPATH=/mnt/shared/modules/all:$MODULEPATH' | sudo tee /etc/profile.d/10-modules.sh
